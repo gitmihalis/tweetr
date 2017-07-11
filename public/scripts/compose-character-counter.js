48c53c6d81cf16681658d020656a95c96addcd69
gitmihalis <mihalisfthenos@icloud.com>
2017-07-11 20:17:41 +0000

    update counter element and make red when invalid

---
+++ public/scripts/compose-character-counter.js
@@ -0,0 +1,19 @@
+console.log("^= compose-character-counter.js loaded successfully")
+$(document).ready(function() {
+  console.log('^= loaded on document ready');
+
+  $('.new-tweet textarea').keyup( function() { 
+    const characterLimit = 140;
+    const characterCount = characterLimit - $(this).val().length;
+    const $counter = $(this).siblings('.counter');
+    $counter.text(characterCount);
+    if (characterCount < 0) {
+      $counter.css({ color: 'darkred'});
+    } else {
+      $counter.css({ color: '#244751'});
+    }
+  });
+
+  
+
+}); // end 
