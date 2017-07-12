344f67cfbd4c87f169165d93a3c351898834914b
gitmihalis <mihalisfthenos@icloud.com>
2017-07-12 21:16:58 +0000

    fix focus/blur bug when new-tweet section is hidden

--- public/scripts/toggle-compose.js
+++ public/scripts/toggle-compose.js
@@ -2,8 +2,15 @@ $(function() {
   // When compse button is clicked, toggle the new-tweet section.
   const $newTweet = $('section.new-tweet');
   $('#compose-btn').on('click', function(e) {
-    $newTweet.slideToggle('fast');
-    $newTweet.find('textarea').focus();
+   
+    if ( $newTweet.is(':hidden') ) {
+      
+      $newTweet.slideToggle('fast');
+      $newTweet.find('textarea').focus();
+    } else {
+      $newTweet.find('textarea').blur();
+       $newTweet.slideToggle('fast');
+    }
   })
 
 });
