32a91d0135c0d7b2a7b99f7c2982ab5f47e18c6d
gitmihalis <mihalisfthenos@icloud.com>
2017-07-13 19:28:36 +0000

    fix hanging flash message

--- public/scripts/toggle-compose.js
+++ public/scripts/toggle-compose.js
@@ -2,6 +2,7 @@ $(function() {
   // When compse button is clicked, toggle the new-tweet section.
   const $newTweet = $('section.new-tweet');
   $('#compose-btn').on('click', function(e) {
+    $('#flash-message').remove();
     if ( $newTweet.is(':hidden') ) {
       $newTweet.slideToggle('fast');
       $newTweet.find('textarea').focus();
