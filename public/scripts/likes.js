aae5da5018704f69ec3cf3cb1246e75811d267c2
gitmihalis <mihalisfthenos@icloud.com>
2017-07-14 18:55:50 +0000

    disable like after tweet is liked on client

--- public/scripts/likes.js
+++ public/scripts/likes.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   console.log('^= likes.js loaded! ')
 
   $("#tweets").on('click', '.icon-list .like', function(event) {
+    const $this = $(this);
     const entityId = $(this).closest("article.tweet").attr('id');
     $.post('/tweets/likes', { entity_id: entityId })
       .done(function() {
+        $this.text('♥︎').removeClass('like').addClass('liked');
         console.log(`Ooo-lala. You like ${entityId} 👩‍❤️‍💋‍👨`);
       })
       .fail(function(err) {
